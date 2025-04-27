51e8c7df690e410bac6521732b6540891ed8493e
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-04-27 15:38:54 +0200

    kubejs

---
+++ kubejs/client_scripts/steel.js
@@ -0,0 +1,10 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! (Loaded server scripts)')
+
+JEIEvents.hideItems(event => {
+    event.hide('ad_astra:steel_ingot')
+})
+

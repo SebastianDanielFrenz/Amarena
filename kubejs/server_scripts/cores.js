99619ec6342d14104c534a862daeaafe42c07af1
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-05-02 11:12:42 +0200

    remove jetpacks + core T1

---
+++ kubejs/server_scripts/cores.js
@@ -0,0 +1,50 @@
+ServerEvents.recipes(event => {
+    event.shaped(
+        Item.of('kubejs:core_t1'),
+        [
+            ' A ',
+            'ATA',
+            ' A '
+        ],
+        {
+            A: 'create:andesite_casing',
+            T: 'create:railway_casing'
+        }
+    )
+    event.shaped(
+        Item.of('kubejs:core_t1'),
+        [
+            ' B ',
+            'BFB',
+            ' B '
+        ],
+        {
+            B: 'minecraft:bread',
+            F: 'minecolonies:flatbread'
+        }
+    )
+    event.shaped(
+        Item.of('kubejs:core_t2'),
+        [
+            ' M ',
+            'MCM',
+            ' M '
+        ],
+        {
+            M: 'botania:manasteel_ingot',
+            C: 'kubejs:core_t1'
+        }
+    )
+    event.shaped(
+        Item.of('kubejs:core_t2'),
+        [
+            ' R ',
+            'RCR',
+            ' R '
+        ],
+        {
+            R: 'bloodmagic:reinforcedslate',
+            C: 'kubejs:core_t1'
+        }
+    )
+})
---
+++ kubejs/startup_scripts/custom_additions.js
@@ -0,0 +1,5 @@
+StartupEvents.registry('minecraft:item', event => {
+    event.create('core_t1')
+    event.create('core_t2')
+    event.create('core_t3')
+})

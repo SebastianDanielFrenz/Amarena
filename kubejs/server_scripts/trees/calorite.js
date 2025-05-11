40de3ec517e44dcb7b11309db608f06bfebb5503
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-05-11 16:03:46 +0200

    calorite

---
+++ kubejs/server_scripts/trees/calorite.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes(event => {
+    event.shaped(
+        Item.of('oretrees:calorite_sapling'),
+        [
+            'AAA',
+            'AOA',
+            ' L '
+        ],
+        {
+            A: 'ad_astra:calorite_block',
+            O: 'minecraft:oak_sapling',
+            L: 'minecraft:oak_log'
+        }
+    )
+})

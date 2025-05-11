932ffdf0c6aec867aa9f59cc28a8d6a661a8cf43
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-05-11 16:03:51 +0200

    ostrum

---
+++ kubejs/server_scripts/trees/ostrum.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes(event => {
+    event.shaped(
+        Item.of('oretrees:ostrum_sapling'),
+        [
+            'AAA',
+            'AOA',
+            ' L '
+        ],
+        {
+            A: 'ad_astra:ostrum_block',
+            O: 'minecraft:oak_sapling',
+            L: 'minecraft:oak_log'
+        }
+    )
+})

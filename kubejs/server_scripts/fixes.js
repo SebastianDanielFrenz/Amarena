78225074e50b97b870d0f095d67e6c0321198233
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2025-05-11 23:16:48 +0200

    Fix pig iron

---
+++ kubejs/server_scripts/fixes.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(event => {
+    event.remove({id: "createaddition:compat/tconstruct/pig_iron"})
+    event.remove({id: "createaddition:compat/tconstruct/pig_iron_2"})
+    event.remove({id: "thermal:compat/tconstruct/smelter_alloy_tconstruct_pigiron_ingot"})
+})
